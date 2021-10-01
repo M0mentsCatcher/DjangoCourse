15ae675331f37dbc9b98145bf542a66ab384c829
Ruslan <mutygullin@yandexlyceum.ru>
2021-10-01 18:48:14 +0300

    added usertags

--- women/admin.py
+++ women/admin.py
@@ -10,10 +10,12 @@ class WomenAdmin(admin.ModelAdmin):
     list_editable = ('is_published',)
     list_filter = ('is_published', 'time_create')
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
+
 
 admin.site.register(Women, WomenAdmin)
 admin.site.register(Category, CategoryAdmin)
